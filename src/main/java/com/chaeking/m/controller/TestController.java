04a1d5088e1a4b6152ea3d97f55a57d049e649ad
jini <jini@jiniworld.me>
2022-07-07 13:17:40 +0900

    :lipstick: 책킹 기본 layout 추가

---
+++ src/main/java/com/chaeking/m/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.chaeking.m.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+    @GetMapping("")
+    public String loginPage() {
+        return "content/chaeking/home";
+    }
+
+}
